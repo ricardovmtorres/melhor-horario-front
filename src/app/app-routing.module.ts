274d9940418f50c34a4c0005e4446faf33a17ae9
Henrique <40470880+henriquebs12@users.noreply.github.com>
2023-03-23 19:11:50 -0300

    Card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GradeComponent } from './components/grade/grade.component';
 import { ListaAlunoComponent } from './components/lista-aluno/lista-aluno.component';
 import { ListaDisciplinaComponent } from './components/lista-disciplina/lista-disciplina.component';
 import { ListaLinhaComponent } from './components/lista-linha/lista-linha.component';
@@ -10,6 +11,7 @@ const routes: Routes = [
   {path: 'linha', component: ListaLinhaComponent},
   {path: 'semestre', component: ListaSemestreComponent},
   {path: 'disciplina', component: ListaDisciplinaComponent},
+  {path: 'grade', component: GradeComponent},
   {path: '', redirectTo: '/aluno', pathMatch:'full'},
 ];
 
